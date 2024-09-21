ff12fbb6878429d38327dbfa20bedf7ae92c104e
ragna <stashmustard@gmail.com>
2024-09-21 18:43:24 +0200

    Initial commit

---
+++ src/main/java/udemy/contactos/servicio/ContactoServicio.java
@@ -0,0 +1,33 @@
+package udemy.contactos.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import udemy.contactos.modelo.Contacto;
+import udemy.contactos.repositorio.IContactoRepositorio;
+
+import java.util.List;
+
+@Service
+public class ContactoServicio implements IContactoServicio{
+    @Autowired
+    IContactoRepositorio contactoRepositorio;
+    @Override
+    public List<Contacto> listarContactos() {
+        return contactoRepositorio.findAll();
+    }
+
+    @Override
+    public Contacto buscarContactoPorId(Integer idContacto) {
+        return contactoRepositorio.findById(idContacto).orElse(null);
+    }
+
+    @Override
+    public void guardarContacto(Contacto contacto) {
+        contactoRepositorio.save(contacto);
+    }
+
+    @Override
+    public void eliminarContacto(Contacto contacto) {
+        contactoRepositorio.delete(contacto);
+    }
+}
